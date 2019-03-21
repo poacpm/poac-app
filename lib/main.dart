907a187953dcbc67cb9f55de640022235b5b003e
matken11235 <26405363+matken11235@users.noreply.github.com>
2019-03-22 05:12:16 +0900

    Change primarySwatch

--- lib/main.dart
+++ lib/main.dart
@@ -4,13 +4,29 @@ import 'package:poac/pages/home.dart';
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
+  final MaterialColor white = const MaterialColor(
+    0xFFFFFFFF,
+    const <int, Color>{
+      50: const Color(0xFFFFFFFF),
+      100: const Color(0xFFFFFFFF),
+      200: const Color(0xFFFFFFFF),
+      300: const Color(0xFFFFFFFF),
+      400: const Color(0xFFFFFFFF),
+      500: const Color(0xFFFFFFFF),
+      600: const Color(0xFFFFFFFF),
+      700: const Color(0xFFFFFFFF),
+      800: const Color(0xFFFFFFFF),
+      900: const Color(0xFFFFFFFF),
+    },
+  );
+
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'poac',
       theme: ThemeData(
-        primarySwatch: Colors.blue,
+        primarySwatch: white,
         canvasColor: Colors.white,
         fontFamily: 'Lato',
       ),
